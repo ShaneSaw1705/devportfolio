64f0d94d7417304001f02813358b0b0cca10fa8d
Shane <shanecurtis1705@gmail.coms>
2024-04-14 19:50:38 +1200

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { NavBar } from "./assets/NavBar";
 import { HeroHeader } from "./assets/HeaderHero";
+import { AboutMe } from "./assets/AboutMe";
 
 export const App = () => {
   return (
     <>
       <NavBar />
       <HeroHeader />
+      <AboutMe />
     </>
   );
 };
