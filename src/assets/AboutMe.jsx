5bf68883b279ef4fa1ebf663e8fdbaca8519e141
Shane <shanecurtis1705@gmail.coms>
2024-04-15 13:12:17 +1200

    im not sure

--- src/assets/AboutMe.jsx
+++ src/assets/AboutMe.jsx
@@ -4,21 +4,16 @@ export const AboutMe = () => {
   return (
     <div className="about-me-container">
       <div className="content">
-        <div>
-          <h1>About me</h1>
-          <p>
-            <h3>Hello!,</h3>
-            My name is Sinan and I specialize in web developement that utilizes
-            HTML, CSS, JS, and REACT etc. I am a highly motivated individual and
-            eternal optimist dedicated to writing clear, concise, robust code
-            that works. Striving to never stop learning and improving. When I'm
-            not coding, I am writing bolgs, reading, or picking up some new
-            hands-on art project like photography. I like to have my perspective
-            and belief systems challenged so that I see the world through new
-            eyes.
-          </p>
+        <div className="header">
+          <div>
+            <h1>About me.</h1>
+            <p>
+              I help business grow by crafting amazing web experiences. If
+              you’re looking for a developer that likes to get stuff done,
+            </p>
+          </div>
+          <img src="" alt="" />
         </div>
-        <img src="" alt="" />
       </div>
     </div>
   );
