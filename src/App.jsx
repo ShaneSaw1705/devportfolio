e5e0692d2db1ceb70190642de151527076ec29f1
Shane <shanecurtis1705@gmail.coms>
2024-04-18 16:24:39 +1200

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { NavBar } from "./assets/NavBar";
 import { HeroHeader } from "./assets/HeaderHero";
 import { AboutMe } from "./assets/AboutMe";
+import { Skills } from "./assets/Skills";
 
 export const App = () => {
   return (
@@ -8,6 +9,7 @@ export const App = () => {
       <NavBar />
       <HeroHeader />
       <AboutMe />
+      <Skills />
     </>
   );
 };
--- src/assets/Skills.jsx
+++ src/assets/Skills.jsx
@@ -1,3 +1,40 @@
+import "../css/Skills.css";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+
 export const Skills = () => {
-  return;
+  return (
+    <div className="skills-container">
+      <div className="content">
+        <div className="skills-header">
+          <div className="skill-title">
+            <h1>Skills</h1>
+            <h3>I strive to never stop learning and improving.</h3>
+            <div className="divider"></div>
+          </div>
+          <div className="platforms"></div>
+          <div className="languages">
+            <h3>
+              <FaHtml5 />
+              Html
+            </h3>
+            <h3>
+              <FaCss3Alt />
+              Css
+            </h3>
+            <h3>
+              <IoLogoJavascript />
+              JS
+            </h3>
+            <h3>
+              <FaReact />
+              React.js
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
